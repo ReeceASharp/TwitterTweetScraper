3b0eed3c5a7027e9c869b0e504e97d484fefb15b
ReeceASharp <ReeceA.sharp@gmail.com>
2020-11-29 11:09:56 -0700

    cleanup + comments

--- TwintEx.py
+++ TwintEx.py
@@ -10,23 +10,27 @@ import os
 # there is downtime between searches as the program outputs the data to file
 # in which case more can be used to speed up output
 class TwintThread(threading.Thread):
-    def __init__(self, year, keywords, output_path):
+    def __init__(self, year, keywords, output):
         threading.Thread.__init__(self)
+
+        # search parameters
         self.year = year
         self.name = self.year
         self.keywords = keywords
-        self.outputPath = "E:\\" + str(self.year) + '\\'
-
         self.limit = 0
         self.min_likes = 0
 
+        # file I/O parameters
+        self.outputPath = output + str(self.year) + '\\'
         self.fileName = "temp/" + str(self.year) + "_current_date.txt"
 
+        # Search config and flag
         self.c = twint.Config()
         self.c.Store_csv = True
         self.c.Hide_output = True
         self.c.Count = True
 
+        # generation of end point, exclusive
         self.end_date = date(self.year + 1, 1, 1)
 
     def run(self):
@@ -46,14 +50,8 @@ class TwintThread(threading.Thread):
             # otherwise, start at the beginning of the year
             current_date = date(self.year, 1, 1)
 
-        # for date_, date_next in date_range(current_date, self.end_date):
-        #     print(date_, date_next)
-
         while current_date < self.end_date:
             for date_, date_next in date_range(current_date, self.end_date):
-
-                # increment current_date for the while conditional above
-
                 try:
                     for keyword in self.keywords:
                         print("{} - {} - {}".format(self.name, date_, keyword))
@@ -74,7 +72,6 @@ class TwintThread(threading.Thread):
                     # write to the file
                     with open(self.fileName, 'w') as file_to_write:
                         file_to_write.write(date_)
-
                 except:
                     # this is used as a fail-safe on network failure
                     # there is a 30 second time-out on Twint, but this
@@ -84,6 +81,7 @@ class TwintThread(threading.Thread):
                     sleep(30)
                     break
                 else:
+                    # update current_date for the while conditional above
                     current_date = date.fromisoformat(date_) + timedelta(days=1)
 
             print("{} != {}".format(current_date, self.end_date))
@@ -98,29 +96,6 @@ def date_range(start_date, end_date):
         # this causes a larger amount of overlap, but that's easy to filter out in Spark/Mapreduce
         next_day = day + timedelta(days=2)
         yield day.strftime("%Y-%m-%d"), next_day.strftime("%Y-%m-%d")
-
-
-if __name__ == '__main__':
-    # search through years with keyword search, creates a thread for each year, could be refactored to
-    # multi-thread on a smaller increment, like a month, week, or day
-    years = list(range(2011, 2021))
-    search = ["tesla", "elonmusk"]
-    output_path = "E:\\"
-
-    threads = []
-    for y in years:
-        t = TwintThread(y, search, output_path)
-        threads.append(t)
-        t.start()
-
-    for t in threads:
-        t.join()
-
-    print("Exiting...")
-    # async cleanup error, possibly an error only on Windows, doesn't affect output and only occurs on thread exit
-    # a possible fix is just to give the graphs time to catch up, in which case a wait-time is used
-    # https://github.com/encode/httpx/issues/914
-    sleep(5)
 
 
 def musk_tweets():
@@ -139,3 +114,28 @@ def musk_tweets():
 
     # start the search
     twint.run.Search(c)
+
+
+if __name__ == '__main__':
+    # search through years with keyword search, creates a thread for each year, could be refactored to
+    # multi-thread on a smaller increment, like a month, week, or day
+    years = list(range(2011, 2021))
+    search = ["tesla", "elonmusk"]
+    output_path = "E:\\"
+
+    # setup multiple threads, one for each year
+    threads = []
+    for y in years:
+        t = TwintThread(y, search, output_path)
+        threads.append(t)
+        t.start()
+
+    # wait until they're all done
+    for t in threads:
+        t.join()
+
+    # async cleanup error, possibly an error only on Windows, doesn't affect output and only occurs on thread exit
+    # a possible fix is just to give the graphs time to catch up, in which case a wait-time is used
+    # https://github.com/encode/httpx/issues/914
+    print("Exiting...")
+    sleep(5)
